1e6afee27a7effcc11e48aa28328d6c99f13318d
Xiang Huang <8440428+xiangyazi24@users.noreply.github.com>
2026-07-06 07:20:14 -0500

    Task 160: import chemFlux Holder frontier

--- ShenWork.lean
+++ ShenWork.lean
@@ -107,6 +107,7 @@ import ShenWork.Paper2.IntervalDomainResolverSupQuantitative
 import ShenWork.Paper2.IntervalDomainCrossDiffusionBootstrap
 import ShenWork.Paper2.IntervalResolverWeakBounds
 import ShenWork.Paper2.IntervalGradientDuhamelMap
+import ShenWork.Paper2.IntervalChemFluxHolderFrontier
 import ShenWork.Paper2.IntervalMildPicard
 import ShenWork.Paper2.IntervalMildRegularityBootstrap
 import ShenWork.Paper2.IntervalDomainL2UEnergyCombine
