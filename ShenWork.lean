bb44ec6e53398aed83fb915f18b64a2f604a3e2b
Xiang Huang <8440428+xiangyazi24@users.noreply.github.com>
2026-07-14 20:38:02 -0500

    root: import recalled paper3 boundedness results

--- ShenWork.lean
+++ ShenWork.lean
@@ -113,6 +113,7 @@ import ShenWork.Paper3.UnitPointLogisticBridge
 import ShenWork.Paper3.UnitPointStatementAssembly
 import ShenWork.Paper3.IntervalDomainTheorem21Part1
 import ShenWork.Paper3.IntervalDomainMinimalFaithfulTheorem22
+import ShenWork.Paper3.IntervalDomainRecalledPropositionsPositive
 import ShenWork.Paper3.IntervalDomainSectorial
 import ShenWork.Paper3.IntervalDomainStabilityChain
 import ShenWork.Paper3.IntervalDomainStatementAssembly
